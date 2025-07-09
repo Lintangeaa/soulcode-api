0340f1c7cf3d188ddb8574c86372fa96fea2b37d
Lintangeaa <lindprak@gmail.com>
2025-07-09 23:28:55 +0700

    add catch all

--- api/index.js
+++ api/index.js
@@ -6,16 +6,19 @@ import { NODE_ENV, PORT } from './libs/index.js';
 const app = express();
 const port = PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.get('/', (req, res) => {
-  res.send('Hello from Express + pnpm + Serverless!');
+  res.send('✅ Hello from Express + Serverless!');
 });
 
 app.use('/email', emailRouter);
+
+// ✅ Tambah catch-all
+app.use((_req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 export default serverless(app);
 
